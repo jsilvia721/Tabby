b0a11ee0aef7c9aab5bfda5cbb42ffe0c90dba20
Josh Silvia <jsilvia@umass.edu>
2016-06-23 09:09:30 -0400

    Switches to new tab after each click of extension button

--- background.js
+++ background.js
@@ -1,16 +1,28 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
   // Send a message to the active tab
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    var activeTab = tabs[0];
+  chrome.tabs.query({currentWindow: true}, function(tabs) {
+  	var foundSelected = false;
+  	for(var i = 0; i < tabs.length; i++){
+  	  if (tabs[i].active){
+        foundSelected = true;
+      }
+      // Finding the next tab.
+      else if (foundSelected){
+        // Selecting the next tab.
+        chrome.tabs.update(tabs[i].id, {active: true});
+        return;
+      }
+  	}
+    // var activeTab = tabs[0];
     chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
   });
 });
 
 // This block is new!
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if( request.message === "open_new_tab" ) {
-      chrome.tabs.create({"url": request.url});
-    }
-  }
-);
+// chrome.runtime.onMessage.addListener(
+//   function(request, sender, sendResponse) {
+//     if( request.message === "open_new_tab" ) {
+//       chrome.tabs.create({"url": request.url});
+//     }
+//   }
+// );
--- content.js
+++ content.js
@@ -1,12 +1,13 @@
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if( request.message === "clicked_browser_action" ) {
-      var firstHref = $("a[href^='http']").eq(0).attr("href");
+      // var firstHref = $("a[href^='http']").eq(0).attr("href");
 
       console.log(firstHref);
 
       // This line is new!
-      chrome.runtime.sendMessage({"message": "open_new_tab", "url": firstHref});
+      // chrome.runtime.sendMessage({"message": "switch_tab"});
+      chrome.tabs.update(tabId, {selected: true});
     }
   }
 );

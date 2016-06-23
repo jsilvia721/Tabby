5808a047876472cd5bcebcfd839d2dabd6d5ebef
Josh Silvia <jsilvia@umass.edu>
2016-06-23 14:46:29 -0400

    added 5 second interval between switches

--- background.js
+++ background.js
@@ -1,29 +1,31 @@
+chrome.browserAction.onClicked.addListener(function(tab) {
 
-chrome.browserAction.onClicked.addListener(function(tabs) {
-  
-  // Send a message to the active tab
-  // function func(){
-  chrome.tabs.query({currentWindow: true}, function(tabs) {
-    var foundSelected = false;
-    for(var i = 0; i <= tabs.length; i++){
-      if(i == tabs.length){
-        i = 0;
-      } 
-      if (tabs[i].active){
-        foundSelected = true;
+  setInterval(function () {
+    chrome.tabs.query({currentWindow: true}, function(tabs) {
+      var foundSelected = false;
+      for(var i = 0; i <= tabs.length+1; i++){
+        if(i == tabs.length){
+          i = 0;
+        }
+        if (tabs[i].active){
+          foundSelected = true;
+        }
+        // Finding the next tab.
+        else if (foundSelected){
+          // Selecting the next tab.
+          chrome.tabs.update(tabs[i].id, {active: true});
+          chrome.tabs.reload(tabs[i].id);
+          return;
+        }
       }
-      // Finding the next tab.
-      else if (foundSelected){
-        // Selecting the next tab.
-        chrome.tabs.update(tabs[i].id, {active: true});
-        chrome.tabs.reload(tabs[i].id);
-        return;
-      }
-    }
-    // var activeTab = tabs[0];
-    chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
-  });
+    });
+  }, 5000);
+  // chrome.browserAction.onClicked.addListener(clear)
+
 });
+
+// var activeTab = tabs[0];
+    // chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
 //   timer = setTimeout(func, 2000);
 // }
 // var timer = setTimeout(func, 2000);
@@ -37,3 +39,60 @@ chrome.browserAction.onClicked.addListener(function(tabs) {
 // );
 // https://www.sitepoint.com/delay-sleep-pause-wait/
 // http://stackoverflow.com/questions/37996288/switch-chrome-tabs-and-refresh-every-5-seconds-javascript
+//   timer = setTimeout(func, 2000);
+// }
+// var timer = setTimeout(func, 2000);
+// This block is new!
+// chrome.runtime.onMessage.addListener(
+//   function(request, sender, sendResponse) {
+//     if( request.message === "open_new_tab" ) {
+//       chrome.tabs.create({"url": request.url});
+//     }
+//   }
+// );
+// https://www.sitepoint.com/delay-sleep-pause-wait/
+// http://stackoverflow.com/questions/37996288/switch-chrome-tabs-and-refresh-every-5-seconds-javascript
+
+
+
+// // chrome.browserAction.onClicked.addListener(function(tabs) {
+  
+//   // Send a message to the active tab
+// chrome.browserAction.onClicked.addListener(function myFunction(tabs) {
+//   setInterval(function(tabs){
+//   chrome.tabs.query({currentWindow: true}, function(tabs) {
+//     var foundSelected = false;
+//     for(var i = 0; i <= tabs.length; i++){
+//       if(i == tabs.length){
+//         i = 0;
+//       } 
+//       if (tabs[i].active){
+//         foundSelected = true;
+//       }
+//       // Finding the next tab.
+//       else if (foundSelected){
+//         // Selecting the next tab.
+//         chrome.tabs.update(tabs[i].id, {active: true});
+//         chrome.tabs.reload(tabs[i].id);
+//         return;
+//       }
+//     }
+//     // var activeTab = tabs[0];
+//     chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
+//   });
+// }, 3000);
+// }
+// });
+// //   timer = setTimeout(func, 2000);
+// // }
+// // var timer = setTimeout(func, 2000);
+// // This block is new!
+// // chrome.runtime.onMessage.addListener(
+// //   function(request, sender, sendResponse) {
+// //     if( request.message === "open_new_tab" ) {
+// //       chrome.tabs.create({"url": request.url});
+// //     }
+// //   }
+// // );
+// // https://www.sitepoint.com/delay-sleep-pause-wait/
+// // http://stackoverflow.com/questions/37996288/switch-chrome-tabs-and-refresh-every-5-seconds-javascript

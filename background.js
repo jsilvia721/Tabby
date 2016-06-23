606ca56bf4f6845753edeae2403cc60c65819705
Josh Silvia <jsilvia@umass.edu>
2016-06-23 13:26:49 -0400

    add refresh after switch

--- background.js
+++ background.js
@@ -1,26 +1,32 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
+
+chrome.browserAction.onClicked.addListener(function(tabs) {
+  
   // Send a message to the active tab
+  // function func(){
   chrome.tabs.query({currentWindow: true}, function(tabs) {
-  	var foundSelected = false;
-  	for(var i = 0; i <= tabs.length; i++){
-  	  if(i == tabs.length){
-  	  	i = 0;
-  	  }	
-  	  if (tabs[i].active){
+    var foundSelected = false;
+    for(var i = 0; i <= tabs.length; i++){
+      if(i == tabs.length){
+        i = 0;
+      } 
+      if (tabs[i].active){
         foundSelected = true;
       }
       // Finding the next tab.
       else if (foundSelected){
         // Selecting the next tab.
         chrome.tabs.update(tabs[i].id, {active: true});
+        chrome.tabs.reload(tabs[i].id);
         return;
       }
-  	}
+    }
     // var activeTab = tabs[0];
     chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
   });
 });
-
+//   timer = setTimeout(func, 2000);
+// }
+// var timer = setTimeout(func, 2000);
 // This block is new!
 // chrome.runtime.onMessage.addListener(
 //   function(request, sender, sendResponse) {
@@ -29,3 +35,5 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 //     }
 //   }
 // );
+// https://www.sitepoint.com/delay-sleep-pause-wait/
+// http://stackoverflow.com/questions/37996288/switch-chrome-tabs-and-refresh-every-5-seconds-javascript
--- content.js
+++ content.js
@@ -1,13 +1,16 @@
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if( request.message === "clicked_browser_action" ) {
-      // var firstHref = $("a[href^='http']").eq(0).attr("href");
-
-      console.log(firstHref);
-
-      // This line is new!
-      // chrome.runtime.sendMessage({"message": "switch_tab"});
+     
       chrome.tabs.update(tabId, {selected: true});
     }
   }
 );
+
+
+ // var firstHref = $("a[href^='http']").eq(0).attr("href");
+
+      // console.log(firstHref);
+
+      // This line is new!
+      // chrome.runtime.sendMessage({"message": "switch_tab"});
